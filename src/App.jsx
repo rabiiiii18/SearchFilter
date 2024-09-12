493f220277e3699e4b262453ff3cfa1d0ff57f40
rabiiiii18 <np03cs4s220260@heraldcollege.edu.np>
2024-09-12 14:02:06 +0545

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,69 @@
+import React from "react";
 import { useState } from "react";
-import { products } from "./data";
+import Task1 from "./assets/component/task1/Task";
+import Task2 from "./assets/component/Task2/Task2";
+import Task3 from "./assets/component/Task3/Task3";
+import Task5 from "./assets/component/Task5/Task5";
 
 function App() {
-  // console.log(products);
-  const [Filter, setFilter] = useState("");
-  console.log(Filter);
-
+  const [selected, setSelected] = useState("null");
+  const renderComponent = () => {
+    if (selected === "task1") {
+      return <Task1 />;
+    } else if (selected === "task2") {
+      return <Task2 />;
+    } else if (selected === "task3") {
+      return <Task3 />;
+    } 
+    
+    else if (selected === "task5") {
+      return <Task5 />;
+    } 
+    else {
+      return <div className="text-xl font-bold flex justify-center items-center">Please select a task</div>;
+    }
+  };
   return (
     <>
-      <div>
-        <div className="flex justify-center items-center mt-32 flex-col">
-          <h1 className="mb-6 text-4xl font-bold">Search Filter App</h1>
-          <input
-            className="h-10 w-[25%] rounded-lg p-5 border-2 border-gray-400"
-            type="text"
-            onChange={(e) => {
-              setFilter(e.target.value);
-            }}
-            placeholder="Type to search"
-          />
-        </div>
-        <div className="flex justify-center items-center mt-10">
-          <div className="relative overflow-x-auto">
-            <table className="w-full text-sm text-left rtl:text-right text-white ">
-              <thead className="text-xs text-white uppercase bg-gray-50 dark:bg-gray-700 ">
-                <tr>
-                  <th scope="col" className="px-6 py-3">
-                    Id
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Name
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Category
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Price
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Rating
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Stock
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Brand
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {products
-                  .filter((item) => {
-                    return item.name.toLowerCase().includes(Filter);
-                  })
-                  .map((item) => (
-                    <tr
-                      class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-                      key={item.id}
-                    >
-                      <td className="px-6 py-3">{item.id}</td>
-                      <td className="px-6 py-3">{item.name}</td>
-                      <td className="px-6 py-3">{item.category}</td>
-                      <td className="px-6 py-3">${item.price}</td>
-                      <td className="px-6 py-3">{item.rating}</td>
-                      <td className="px-6 py-3">{item.stock}</td>
-                      <td className="px-6 py-3">{item.brand}</td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
+      <div className="">
+        <div className="flex justify-center items-center flex-col mt-24 bg-white mx-4 rounded-lg h-full">
+          <div>
+            <h1 className="text-4xl font-bold font-serif">My Tasks</h1>
+          </div>
+          <div className="mt-10 ">
+            <button
+              type="button"
+              onClick={() => setSelected("task1")}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-6"
+            >
+              Task1
+            </button>
+            <button
+              type="button"
+              onClick={() => setSelected("task2")}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-6"
+            >
+              Task2
+            </button>
+            <button
+              type="button"
+              onClick={() => setSelected("task3")}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-6"
+            >
+              Task3
+            </button>
+            
+            <button
+              type="button"
+              onClick={() => setSelected("task5")}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-6"
+            >
+              Task5
+            </button>
+          </div>
+          <div className="h-[50%] w-[60%] mt-10 rounded-xl">
+            {/* <Task1 /> */}
+            {renderComponent()}
           </div>
         </div>
       </div>
